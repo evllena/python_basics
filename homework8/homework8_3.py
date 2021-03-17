36e104beb40128fff1a2089288c0b2a231ffc1b7
evllena <helen.ermolaeva7@gmail.com>
2021-03-17 18:48:12 +1100

    Небольшие изменения в задании 3

--- homework8/homework8_3.py
+++ homework8/homework8_3.py
@@ -3,14 +3,27 @@ class MyValueError(Exception):
         self.text = text
 
 
+def is_number(string):
+    if string.isdigit():
+        return True
+    else:
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
+
+
 new_list = []
 while True:
     item = input("Введите целое число или F для завершения: ")
     if item == 'F':
-        print(new_list)
+        print(f"Так выглядит ваш список: ")
+        for n in new_list:
+            print(n, end=' ')
         break
     try:
-        if not item.isdigit():
+        if not is_number(item):
             raise MyValueError('Значение не будет добавлено в список, так как не является целым числом!')
     except MyValueError as my_error:
         print(my_error)
